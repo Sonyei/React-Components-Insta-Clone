b74e1ef30076430b788339993db4c4efc7095acc
Sonyei <esonyei@gmail.com>
2020-10-06 18:05:28 -0400

    completed post & like

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -2,13 +2,17 @@ import React from 'react';
 import Comment from './Comment';
 import './Comments.css';
 
-const Comments = props => {
+const Comments = (props) => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
 
+
+
   return (
     <div>
-      {/* map through the comments prop and render a Comment for every piece of data */}
+      {/* {postObj.comments.map(text => {
+        return <div key={comments}>{comments}</div>
+      })} */}
     </div>
   );
 };
--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -5,25 +5,24 @@ import PostHeader from './PostHeader';
 
 const Post = props => {
   // 🔥 Make sure the parent of Post is passing the right props!
-  const { post, likePost } = props;
+  const { postDetails, likeButton} = props;
 
   return (
     <div className='post-border'>
       <PostHeader
-        username={post.username}
-        thumbnailUrl={post.thumbnailUrl}
+        username={postDetails.username}
+        thumbnailUrl={postDetails.thumbnailUrl}
       />
       <div className='post-image-wrapper'>
         <img
           alt='post thumbnail'
           className='post-image'
-          src={post.imageUrl}
+          src={postDetails.imageUrl}
         />
       </div>
-      {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likePost={() => likePost(post.id)} />
-      {/* Comments also wants its props! */}
-      <Comments />
+      
+      <LikeSection likeButton={evt => likeButton(postDetails.id)} numberOfLikes={postDetails.likes}/>)
+      <Comments comment={postDetails.comments}/>
     </div>
   );
 };
